6cfacae9ca48af90ecba7568fc69526d45258039
Kristin <knhbjr@gmail.com>
2026-05-16 16:49:56 -0500

    Set up Express, Prisma, and logging

---
+++ server.ts
@@ -0,0 +1,27 @@
+import { PrismaClient } from "@prisma/client";
+import express from "express";
+import pino from "pino";
+import PinoHttp from "pino-http";
+import cors from "cors";
+
+// Creates logger instance
+const logger = pino();
+
+// Creates the Express app
+const app = express();
+
+// Requests from this URL is allowed - for React
+app.use(cors({ origin: "http://localhost:5173"}));
+// Parses incoming requests
+app.use(express.json());
+
+// Logs request information
+app.use(PinoHttp({ 
+  logger,
+  autoLogging: process.env.NODE_ENV === "production"
+}));
+
+const port = 3001;
+
+// Creates the Prisma client instance - queries the database.
+const prisma = new PrismaClient();
