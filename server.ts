910719a640e8676e9ae56e1fd56af2745d71bd27
Kristin <knhbjr@gmail.com>
2026-05-16 17:50:31 -0500

    Moved the const variables to all be grouped together

--- server.ts
+++ server.ts
@@ -15,6 +15,11 @@ const logger = pino();
 // Creates the Express app
 const app = express();
 
+const port = 3001;
+
+// Creates the Prisma client instance - queries the database.
+const prisma = new PrismaClient();
+
 // Requests from this URL is allowed - for React
 app.use(cors({ origin: "http://localhost:5173"}));
 // Parses incoming requests
@@ -26,14 +31,10 @@ app.use(PinoHttp({
   autoLogging: process.env.NODE_ENV === "production"
 }));
 
-const port = 3001;
-
-// Creates the Prisma client instance - queries the database.
-const prisma = new PrismaClient();
 
 app.use("/species", speciesRoutes);
 app.use("/breeds", breedRoutes);
-app.use("animals", animalRoutes);
+app.use("/animals", animalRoutes);
 
 
 // Starts the server and listens for incoming requests.
