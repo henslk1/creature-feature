f1b7d43ce2dc509ace6a9bd8e61dc2b8aaeff2a9
Kristin <knhbjr@gmail.com>
2026-05-17 17:04:51 -0500

    Get all species present.

--- routes/species.ts
+++ routes/species.ts
@@ -4,5 +4,23 @@ import logger from "../lib/logger";
 
 const router = Router();
 
+// GET all species
+router.get("/species", async (req, res) => {
+
+  try {
+    const getSpecies = await prisma.species.findMany({
+      orderBy: { id: "asc"}
+    });
+
+    logger.info({ species: getSpecies }, "species found");
+    res.json(getSpecies);
+  }
+  
+  catch(error) {
+    logger.error({ error }, "Internal server error");
+    res.status(500).json({ message: "Internal server error" });
+  }
+  
+})
 
 export default router;
