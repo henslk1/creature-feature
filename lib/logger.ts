9f46285eab163a68073db63f53e2ba0eb2480d13
Kristin <knhbjr@gmail.com>
2026-05-17 17:47:35 -0500

    Added pino pretty to format the logger output.

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,5 +1,15 @@
 import pino from "pino";
 
 // Creates logger instance
-const logger = pino();
+const logger = pino({
+  transport: {
+    target: "pino-pretty",
+    options: {
+      colorize: true,
+      translateTime: "SYS:standard",
+      ignore: "pid,hostname"
+    }
+  }
+});
+
 export default logger;
