59659da7ac6c1be6423d44e798b2896bbd696e6c
Kristin <knhbjr@gmail.com>
2026-05-17 20:07:02 -0500

    Split server.ts into two files so the app can be used in the server and in testing

---
+++ app.ts
@@ -0,0 +1,28 @@
+import logger from "./lib/logger";
+import express from "express";
+import PinoHttp from "pino-http";
+import cors from "cors";
+// Import the routes
+import speciesRoutes from "./routes/species";
+import breedRoutes from "./routes/breeds";
+import animalRoutes from "./routes/animals";
+
+// Creates the Express app
+const app = express();
+
+// Requests from this URL is allowed - for React
+app.use(cors({ origin: "http://localhost:5173"}));
+// Parses incoming requests
+app.use(express.json());
+
+// Logs request information
+app.use(PinoHttp({ 
+  logger,
+  autoLogging: process.env.NODE_ENV === "production"
+}));
+
+app.use("/species", speciesRoutes);
+app.use("/breeds", breedRoutes);
+app.use("/animals", animalRoutes);
+
+export default app;
--- server.ts
+++ server.ts
@@ -1,33 +1,8 @@
-// Import middleware
+// Import app and logger
+import app from "./app";
 import logger from "./lib/logger";
-import express from "express";
-import PinoHttp from "pino-http";
-import cors from "cors";
-// Import the routes
-import speciesRoutes from "./routes/species";
-import breedRoutes from "./routes/breeds";
-import animalRoutes from "./routes/animals";
-
-// Creates the Express app
-const app = express();
 
 const port = 3001;
-
-// Requests from this URL is allowed - for React
-app.use(cors({ origin: "http://localhost:5173"}));
-// Parses incoming requests
-app.use(express.json());
-
-// Logs request information
-app.use(PinoHttp({ 
-  logger,
-  autoLogging: process.env.NODE_ENV === "production"
-}));
-
-app.use("/species", speciesRoutes);
-app.use("/breeds", breedRoutes);
-app.use("/animals", animalRoutes);
-
 
 // Starts the server and listens for incoming requests.
 app.listen(port, () => {
