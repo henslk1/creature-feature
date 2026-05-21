b4cf58589ee3f9a8539118a3acbe6b59198237e9
Kristin <knhbjr@gmail.com>
2026-05-20 21:31:37 -0500

    Created the species object to use in testing

--- tests/species.test.ts
+++ tests/species.test.ts
@@ -4,4 +4,16 @@ import prisma from "../lib/prisma";
 
 describe("Species routes", () => {
 
+  // ID for object created during testing.
+  let createdId: number;
+
+  // Object to be used in testing
+  beforeAll(async () => {
+    const response = await request(app)
+    .post("/species")
+    .send({ name: "Gremlin", description: "Mythical creature" });
+  createdId = response.body.id;
+  });
+
+  
 });
