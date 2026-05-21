a7428c3042a6e8b4d0addbd808c4f23f99256b32
Kristin <knhbjr@gmail.com>
2026-05-20 22:01:00 -0500

    Get array of breeds

--- routes/breeds.ts
+++ routes/breeds.ts
@@ -4,5 +4,22 @@ import logger from "../lib/logger";
 
 const router = Router();
 
+// GET all breeds
+router.get("/", async (req, res) => {
+  
+  try {
+    const getBreeds = await prisma.breed.findMany({
+      orderBy: { id: "asc" }
+    });
+
+    logger.info({ breeds: getBreeds }, "Breeds found");
+    res.json(getBreeds);
+  }
+
+  catch(error) {
+    logger.error({ error }, "Internal server error");
+    res.status(500).json({ message: "Internal server error" });
+  }
+})
 
 export default router;
