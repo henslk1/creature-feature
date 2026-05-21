43c89fefc94759ec2a87e5ef4da8663ac26eac91
Kristin <knhbjr@gmail.com>
2026-05-20 21:28:52 -0500

    Restructuring the test, removed old code and imported prisma

--- tests/species.test.ts
+++ tests/species.test.ts
@@ -1,19 +1,7 @@
 import request from "supertest";
 import app from "../app";
+import prisma from "../lib/prisma";
 
 describe("Species routes", () => {
 
-  it("should return a list of species", async() => {
-    const response = await request(app).get("/species");
-    expect(response.status).toBe(200);
-  });
-
-  it("should create a new species", async () => {
-    const response = await request(app)
-      .post("/species")
-      .send({ name: "Dog", description: "Canine species" });
-    expect(response.status).toBe(200);
-    expect(response.body.name).toBe("Dog");
-  })
-  
 });
