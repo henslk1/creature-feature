bfae93af5bbe5e8d4216a96f23c875c3074eac5c
Kristin <knhbjr@gmail.com>
2026-05-21 21:24:40 -0500

    Clean up species and breeds created during testing

--- tests/helpers.ts
+++ tests/helpers.ts
@@ -7,3 +7,12 @@ export async function createTestSpecies() {
     .send({ name: "Test Species", description: "Test Species Description" });
   return response.body;
 }
+
+// Test clean up
+export async function deleteTestSpecies(id: number) {
+  await request(app).delete(`/species/${id}`);
+}
+
+export async function deleteTestBreed(id: number) {
+  await request(app).delete(`/breeds/${id}`);
+}
