d6e118b3a71b7dfb32bd5b7ab4bc58c6673021f9
Kristin <knhbjr@gmail.com>
2026-05-21 22:12:13 -0500

    Disabled concurrent testing

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     globals: true,
-    sequence: { concurrent: false },
+    fileParallelism: false,
   },
 });
