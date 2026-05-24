64ca191b574dc1d641e666663508d1af93ad6d55
Kristin <knhbjr@gmail.com>
2026-05-24 17:11:40 -0500

    Updated helper functions for the testing - helpers return the full response now.

--- tests/helpers.ts
+++ tests/helpers.ts
@@ -2,24 +2,21 @@ import request from "supertest";
 import app from "../app";
 
 export async function createTestSpecies() {
-  const response = await request(app)
+  return await request(app)
     .post("/species")
     .send({ name: "Test Species" });
-  return response.body;
 }
 
 export async function createTestBreed(speciesId: number) {
-  const response = await request(app)
+  return await request(app)
     .post("/breeds")
     .send({ name: "Test Breed", speciesId });
-  return response.body;
 }
 
 export async function createTestAnimal(breedId: number) {
-  const response = await request(app)
+  return await request(app)
     .post("/animals")
     .send({ name: "Test animal", breedId });
-  return response.body;
 }
 
 // Test clean up
