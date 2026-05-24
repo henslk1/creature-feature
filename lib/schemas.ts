f81eebe0edafb40085d47b1effdc49d57738a2b3
Kristin <knhbjr@gmail.com>
2026-05-24 17:34:46 -0500

    Allele schema

--- lib/schemas.ts
+++ lib/schemas.ts
@@ -9,3 +9,12 @@ export const breedSchema = z.object({
   name: z.string().min(1),
   speciesId: z.number().int().positive(),
 });
+
+export const alleleSchema = z.object({
+  name: z.string().min(1),
+  symbol: z.string().min(1),
+  dominance: z.string().min(1),
+  probability: z.number().max(1),
+})
+
+
