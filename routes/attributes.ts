82a567d6821e3cb6a580fa3be1e770b95b9ad181
Kristin <knhbjr@gmail.com>
2026-05-26 20:07:57 -0500

    Route file for attributes

---
+++ routes/attributes.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import prisma from "../lib/prisma";
+import logger from "../lib/logger";
+import { validate } from "../lib/validate";
+import { attributeDefinitionSchema } from "../lib/schemas";
+
+const router = Router({ mergeParams: true });
+
+// GET attribute
+
+// POST attribute
+
+// PATCH attribute
+
+// DELETE attribute
+
+export default router;
