55a45bbcc953bf0bd1dc19ac17ec6624b2e222b2
Kristin <knhbjr@gmail.com>
2026-05-26 20:14:39 -0500

    GET Attribute

--- routes/attributes.ts
+++ routes/attributes.ts
@@ -7,6 +7,29 @@ import { attributeDefinitionSchema } from "../lib/schemas";
 const router = Router({ mergeParams: true });
 
 // GET attribute
+router.get("/:attributeDefinitionId", async (req, res) => {
+
+  try {
+    const found = await prisma.gene.findUnique({
+      where: { id: Number(req.params.attributeDefinitionId) },
+    })
+
+    if (!found) {
+      logger.warn({ attributeDefinitionId: req.params.attributeDefinitionId },
+        `Attribute [${req.params.attributeDefinitionId}] not found`);
+      res.status(404).json({ message: "Attribute not found" });
+      return;
+    }
+
+    logger.info({ attribute: found }, "Attribute found");
+    res.json(found);
+  }
+
+  catch (error) {
+    logger.error({ error }, "Internal server error");
+    res.status(500).json({ message: "Internal server error" });
+  }
+})
 
 // POST attribute
 
