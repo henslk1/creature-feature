c4101235c8974776e58e7ff01f5b9e66a1b40237
Kristin <knhbjr@gmail.com>
2026-05-28 01:34:47 -0500

    getRange and rollInRange handlers

--- lib/rollUtils.ts
+++ lib/rollUtils.ts
@@ -7,6 +7,16 @@ export function weightedRandom<T extends { probability: number }>(items: T[]) {
   return items[items.length - 1];
 }
 
+export function getRange(stat: any, statRanges: any[]): { min: number, max: number } {
+  const range = statRanges.find(r =>r.stat === stat.name);
+  return range ?? { min: stat.min, max: stat.max };
+}
+
+export function rollInRange(min: number, max: number) {
+  let result = Math.random() * (max - min) + min;
+  return result;
+}
+
 export function applyOverrides(alleles: any[], overrides: any[]) {
   return alleles.map(allele => {
     const override = overrides.find(o => o.alleleId === allele.id);
