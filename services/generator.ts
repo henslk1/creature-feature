3bcd1457139301cf9a3def0d0145c9755d60feba
Kristin <knhbjr@gmail.com>
2026-05-28 14:03:19 -0500

    Roll all fields and return the generated animal. Removed logger and prisma imports. Logs will be handled with routes, prisma isn't needed anymore. Removed the try{} and catch{}, it will also be handled in the routes

--- services/generator.ts
+++ services/generator.ts
@@ -1,20 +1,18 @@
-import prisma from "../lib/prisma";
-import logger from "../lib/logger";
+import { fetchBreedProfile } from "./queries";
 import { rollAttributes } from "./rollAttributes";
 import { rollGenes } from "./rollGenes";
 import { rollStats } from "./rollStats";
 
 export async function generateAnimal(breedId: number) {
+  const breedData = await fetchBreedProfile(breedId);
 
-  try {
-    const breedData = await prisma.breed.findUnique({
-      where: { id: breedId}
-    })
-
-    if (!breedData) {
-      logger.warn({ breedId }, `Breed [${breedId}] not found`);
-      throw new Error(`Breed [${breedId}] not found`);
-    }
-    
+  if (!breedData) {
+    throw new Error(`Breed [${breedId}] not found`);
   }
+
+  const stats = rollStats(breedData.species.stats, breedData.statRanges);
+  const attributes = rollAttributes(breedData.species.attributes);
+  const expressedTraits = rollGenes(breedData.species.genes, breedData.overrides);
+
+  return { stats, attributes, expressedTraits };
 }
