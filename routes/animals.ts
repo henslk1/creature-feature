5ebf0c6d526190fe3035a21c3df2e5ad7fccffab
Kristin <knhbjr@gmail.com>
2026-05-28 14:10:12 -0500

    GET all animals

--- routes/animals.ts
+++ routes/animals.ts
@@ -1,8 +1,28 @@
 import { Router } from "express";
 import prisma from "../lib/prisma";
 import logger from "../lib/logger";
+import { validate } from "../lib/validate";
+//import { animalSchema } from "../lib/validate";
+import { generateAnimal } from "../services/generator";
 
 const router = Router();
+
+router.get("/", async (req, res) => {
+
+  try {
+    const getAnimals = await prisma.animal.findMany({
+      orderBy: { id: "asc" }
+    });
+
+    logger.info({ animals: getAnimals }, "Animals found");
+    res.json(getAnimals);
+  }
+
+  catch (error) {
+    logger.error({ error }, "Internal server error");
+    res.status(500).json({ message: "Internal server error" });
+  }
+})
 
 
 export default router;
