4b7b775098493c872291ef5e3525ff1d7fa77dd8
Kristin <knhbjr@gmail.com>
2026-05-28 14:59:31 -0500

    Initialized objects for testing

--- tests/animals.test.ts
+++ tests/animals.test.ts
@@ -13,8 +13,27 @@ import {
  describe("Animal routes", () => {
 
   // ID for object created during testing and base path definition
+  let speciesId: number;
+  let breedId: number;
+  let animalId: number;
+  let animalCreateStatus: number;
 
   // Object to be used in testing
+  beforeAll( async () => {
+    const speciesResponse = await createTestSpecies();
+    speciesId = speciesResponse.body.id;
+
+    const breedResponse = await createTestBreed(speciesId);
+    breedId = breedResponse.body.id
+
+    await createTestGene(speciesId);
+    await createTestStat(speciesId);
+    await createTestAttribute(speciesId);
+
+    const animalResponse = await createTestAnimal(breedId);
+    animalId = animalResponse.body.id;
+    animalCreateStatus = animalResponse.status;
+  })
 
   // Create a new animal
 
@@ -32,4 +51,3 @@ import {
   })
 
  });
- 
