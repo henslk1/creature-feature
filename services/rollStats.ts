c50ad37bb0f883de80bcc9267ff4df87c7ba1c28
Kristin <knhbjr@gmail.com>
2026-05-28 01:48:37 -0500

    Roll stats

---
+++ services/rollStats.ts
@@ -0,0 +1,12 @@
+import { getRange, rollInRange } from "../lib/rollUtils";
+
+export function rollStats(stats: any[], statRanges: any[]): Record<string, number> {
+  const finalStats: Record<string, number> = {};
+
+  for (const stat of stats) {
+    const { min, max } = getRange(stat, statRanges);
+    finalStats[stat.name] = rollInRange(min, max);
+  }
+
+  return finalStats;
+}
