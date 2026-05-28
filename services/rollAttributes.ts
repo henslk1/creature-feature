5fd8043641792e81bcbc2116dfb3c5ec59d317f5
Kristin <knhbjr@gmail.com>
2026-05-28 02:03:07 -0500

    Roll attributes

---
+++ services/rollAttributes.ts
@@ -0,0 +1,23 @@
+import { rollEnum, rollInRange, rollString } from "../lib/rollUtils";
+
+export function rollAttributes(attributes: any[]): Record<string, any> {
+  const result: Record<string, any> = {};
+
+  for (const attribute of attributes) {
+
+    switch (attribute.type) {
+      case "number": 
+        result[attribute.name] = rollInRange(attribute.min, attribute.max);
+        break;
+      case "string": 
+        result[attribute.name] = rollString();
+        break;
+      case "enum":
+        result[attribute.name] = rollEnum(attribute.options);
+        break;
+
+    }
+  }
+
+  return result;
+}
