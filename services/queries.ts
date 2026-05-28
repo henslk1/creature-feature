44608885f43112b058a1bdf1c55574018e4f9f48
Kristin <knhbjr@gmail.com>
2026-05-28 13:52:05 -0500

    Separated the fetch breed logic into it's own file

---
+++ services/queries.ts
@@ -0,0 +1,26 @@
+import prisma from "../lib/prisma";
+
+
+export async function fetchBreedProfile(breedId: number) {
+
+  return await prisma.breed.findUnique({
+      where: { id: breedId },
+      include: {
+        statRanges: true,
+        overrides: true,
+        species: {
+          include: {
+            stats: true,
+            attributes: true,
+            genes: {
+              include: {
+                loci: { include: {alleles: true } },
+                expressionRules: true
+              }
+            }
+          }
+        }
+      }
+  })
+
+}
