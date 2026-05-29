7eb2e753c7a89072c7325492b9fff9f2010dc671
Kristin <knhbjr@gmail.com>
2026-05-28 23:13:02 -0500

    Updated imports so it only imports weightedRandom

--- tests/utils/weightedRandom.test.ts
+++ tests/utils/weightedRandom.test.ts
@@ -1,22 +1,22 @@
-import * as rollUtils from "../../lib/rollUtils";
+import { weightedRandom } from "../../lib/rollUtils";
 import { testAlleles, singleItem } from "./testData";
 
 describe("weightedRandom", () => {
 
   it("should return an item from the array", () => {
-    const result = rollUtils.weightedRandom(testAlleles);
+    const result = weightedRandom(testAlleles);
     expect(testAlleles).toContain(result);
   });
 
   it("should always return the only item when probability is 1", () => {
-    const result = rollUtils.weightedRandom(singleItem);
+    const result = weightedRandom(singleItem);
     expect(result).toBeDefined();
     if (result) expect(result.name).toBe("A");
   });
 
   it("never returns an item with probability of 0", () => {
     for (let i = 0; i < 1000; ++i) {
-      const result = rollUtils.weightedRandom(testAlleles);
+      const result = weightedRandom(testAlleles);
       if (result) expect(result.name).not.toBe("C");
     }
   });
