c5e53c09a8362ef9d21cfcf7bc2cb9c53ac62a7f
Kristin <knhbjr@gmail.com>
2026-05-29 15:13:10 -0500

    Test stats, test range, and an empty test range

---
+++ tests/services/testData.ts
@@ -0,0 +1,10 @@
+export const testStatsArray = [
+  { name: "StatA", min: 0, max: 100 },
+  { name: "StatB", min: 10, max: 80}
+];
+
+export const testStatRanges = [
+  { stat: "StatA", min: 50, max: 75 }
+];
+
+export const emptyStatRanges = [];
