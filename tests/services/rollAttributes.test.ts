c90bdeae5a7de82785bf1d3f1d5595a14bf19c97
Kristin <knhbjr@gmail.com>
2026-05-29 17:12:30 -0500

    enum and string tests

--- tests/services/rollAttributes.test.ts
+++ tests/services/rollAttributes.test.ts
@@ -9,5 +9,17 @@ describe("rollAttributes", () => {
     expect(result["numberAttr"]).toBeLessThanOrEqual(testAttributes.numAttr.max);
     expect(typeof result["numberAttr"]).toBe("number");
   });
+
+  it("should roll and pick one option in a given enum", () => {
+    const result = rollAttributes([testAttributes.enumAttr]);
+    expect(testAttributes.enumAttr).toContain(result["enumAttr"]);
+    expect(typeof result["enumAttr"]).toBe("string");
+  });
+
+  it("should return an empty string when a string is given", () => {
+    const result = rollAttributes([testAttributes.stringAttr]);
+    expect(result["stringAttr"]).toBe("");
+    expect(typeof result["stringAttr"]).toBe("string");
+  });
   
 })
