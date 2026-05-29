c3bb82764454650e3c248ef9124a6ebf7577fe78
Kristin <knhbjr@gmail.com>
2026-05-29 17:17:18 -0500

    Test gene

--- tests/services/testData.ts
+++ tests/services/testData.ts
@@ -14,3 +14,22 @@ export const testAttributes = {
   enumAttr: { name: "enumAttr", type: "enum", options: ["1", "2", "3"] },
   stringAttr: { name: "stringAttr", type: "string" }
 };
+
+export const testGene = {
+  name: "Test Gene",
+  category: "Color",
+  loci: [
+    {
+      name: "Test Locus",
+      alleles: [
+        { id: 1, name: "Allele A", symbol: "A", dominance: "dominant", probability: 0.5 },
+        { id: 2, name: "Allele a", symbol: "a", dominance: "recessive", probability: 0.5}
+      ]
+    }
+  ],
+  expressionRules: [
+    { minDominantAlleles: 0, expression: "No Expression" },
+    { minDominantAlleles: 1, expression: "Test Expression" },
+    { minDominantAlleles: 2, expression: "Full Expression"}
+  ]
+}
