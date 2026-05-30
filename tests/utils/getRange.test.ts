282e1408f3e631946408359aa1b5719bab350e88
Kristin <knhbjr@gmail.com>
2026-05-30 13:11:55 -0500

    Added max number checks

--- tests/utils/getRange.test.ts
+++ tests/utils/getRange.test.ts
@@ -3,19 +3,22 @@ import { testStats, testStatRanges } from "./testData";
 
 describe("getRange", () => {
 
-  it("should return 50 for Stat A", () => {
+  it("should return 50-100 for Stat A", () => {
     const result = getRange(testStats.statA, testStatRanges.withMatch);
     expect(result.min).toBe(50);
+    expect(result.max).toBe(100);
   });
 
-  it("should return 50 for Stat B", () => {
+  it("should return 0 - 100 for Stat B", () => {
     const result =getRange(testStats.statB, testStatRanges.noMatch);
     expect(result.min).toBe(0);
+    expect(result.max).toBe(100);
   });
 
-  it("should return min: 25 for Stat B", () => {
+  it("should return 25 - 100 for Stat B", () => {
     const result = getRange(testStats.statB, testStatRanges.doubleRange);
     expect(result.min).toBe(25);
+    expect(result.max).toBe(100);
   });
 
 })
