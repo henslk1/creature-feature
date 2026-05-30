c96a8101a2010726f37c4435cce7c1b34ada5251
Kristin <knhbjr@gmail.com>
2026-05-30 17:38:45 -0500

    Gene validation

---
+++ tests/routes/validation/genes.validation.test.ts
@@ -0,0 +1,45 @@
+import request from "supertest";
+import app from "../../../app";
+import { createTestGene, createTestSpecies, deleteTestSpecies } from "../helpers";
+
+describe("Gene validation", () => {
+
+  let duplicateId: number;
+  let speciesId: number;
+  let basePath: string;
+
+  const invalidGeneId = 999999;
+  const duplicateGeneName = "Validate Gene";
+  const emptyName = "";
+  const testSpeciesName = "Validate Species";
+
+  beforeAll( async () => {
+    const speciesResponse = await createTestSpecies(testSpeciesName);
+    speciesId = speciesResponse.body.id;
+
+    basePath = `/species/${speciesId}/genes`
+
+    const duplicateGene = await createTestGene(speciesId, duplicateGeneName);
+    duplicateId = duplicateGene.body.id;
+  });
+
+  it("should return 404 for gene that does not exist", async () => {
+    const response = await request(app).get(`${basePath}/${invalidGeneId}`);
+    expect(response.status).toBe(404);
+  });
+
+  it("should returnn 409 when creating a gene with a duplicate name", async () => {
+    const response = await createTestGene(speciesId, duplicateGeneName);
+    expect (response.status).toBe(409);
+  });
+
+  it("shoud return 400 when creating a gene with invalid data", async () => {
+    const response = await createTestGene(speciesId, emptyName);
+    expect(response.status).toBe(400);
+  });
+
+  afterAll( async () => {
+    await deleteTestSpecies(speciesId);
+  })
+
+});
