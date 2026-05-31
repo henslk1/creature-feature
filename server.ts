ed8e9c1823086d70b542d6036e746d84cdc03ef7
Kristin <knhbjr@gmail.com>
2026-05-30 23:04:51 -0500

    Prisma disconnect handlers

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 // Import app and logger
 import app from "./app";
 import logger from "./lib/logger";
+import prisma from "./lib/prisma";
 
 const port = 3001;
 
@@ -8,3 +9,11 @@ const port = 3001;
 app.listen(port, () => {
   logger.info({ port }, "Creature feature app listening on");
 });
+
+const shutdown = async () => {
+  await prisma.$disconnect();
+  process.exit(0);
+};
+
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
