f3a0e8e989a12f87d6aa7cfb1aba37f247b60081
Kristin <knhbjr@gmail.com>
2026-06-01 00:18:56 -0500

    Species get useState and useEffect

--- frontend/src/pages/SpeciesPage.tsx
+++ frontend/src/pages/SpeciesPage.tsx
@@ -1,5 +1,25 @@
+import { useState, useEffect } from "react";
+
+interface Species {
+  id: number,
+  name: string,
+  description: string
+}
+
 export function SpeciesPage() {
+
+  const API_URL = "http://localhost:3001";
+
+  // GET
+  const [species, setSpecies] = useState<Species[]>([]);
+
+  useEffect(() => {
+    fetch(`${API_URL}/species`)
+        .then(res => res.json())
+        .then(data => setSpecies(data))
+  }, [])
+
   return (
-    <div>Species</div>
+    <h1>Species</h1>
   )
 }
