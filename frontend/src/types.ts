ae18d3143090b4d3a2f7baf6858b002b8a19236b
Kristin <knhbjr@gmail.com>
2026-06-01 01:57:07 -0500

    Empty interfaces to use in pages

---
+++ frontend/src/types.ts
@@ -0,0 +1,35 @@
+export interface Allele {
+
+}
+
+export interface Locus {
+
+}
+
+export interface ExpressionRule {
+
+}
+
+export interface Gene {
+
+}
+
+export interface Stat {
+
+}
+
+export interface Attribute {
+
+}
+
+export interface Animal {
+
+}
+
+export interface Breed {
+
+}
+
+export interface Species {
+
+}
