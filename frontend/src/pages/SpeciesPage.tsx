226f178fc71920d4016a33336d96c79f7f08e56f
Kristin <knhbjr@gmail.com>
2026-06-01 00:39:42 -0500

    Species table

--- frontend/src/pages/SpeciesPage.tsx
+++ frontend/src/pages/SpeciesPage.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 interface Species {
   id: number,
@@ -9,6 +10,7 @@ interface Species {
 export function SpeciesPage() {
 
   const API_URL = "http://localhost:3001";
+  const navigate = useNavigate();
 
   // GET
   const [species, setSpecies] = useState<Species[]>([]);
@@ -20,6 +22,25 @@ export function SpeciesPage() {
   }, [])
 
   return (
-    <h1>Species</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Description</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {species.map(s => (
+          <tr key={s.id}>
+            <td>{s.id}</td>
+            <td>{s.name}</td>
+            <td>{s.description}</td>
+            <td><button onClick={() => navigate(`/species/${s.id}`)}>View</button></td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   )
 }
