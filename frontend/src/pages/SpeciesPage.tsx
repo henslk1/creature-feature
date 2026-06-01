c53036388c6c034b5d5a9a4380bb1b4b713984d7
Kristin <knhbjr@gmail.com>
2026-06-01 00:55:40 -0500

    Wrapped the contents in a <div>, added the add species toggle

--- frontend/src/pages/SpeciesPage.tsx
+++ frontend/src/pages/SpeciesPage.tsx
@@ -15,6 +15,9 @@ export function SpeciesPage() {
   // GET
   const [species, setSpecies] = useState<Species[]>([]);
 
+  // Add species handler
+  const [showAddForm, setShowAddForm] = useState(false);
+
   useEffect(() => {
     fetch(`${API_URL}/species`)
         .then(res => res.json())
@@ -22,25 +25,39 @@ export function SpeciesPage() {
   }, [])
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Name</th>
-          <th>Description</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        {species.map(s => (
-          <tr key={s.id}>
-            <td>{s.id}</td>
-            <td>{s.name}</td>
-            <td>{s.description}</td>
-            <td><button onClick={() => navigate(`/species/${s.id}`)}>View</button></td>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Description</th>
+            <th></th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+         {species.map(s => (
+            <tr key={s.id}>
+              <td>{s.id}</td>
+              <td>{s.name}</td>
+             <td>{s.description}</td>
+             <td><button onClick={() => navigate(`/species/${s.id}`)}>View</button></td>
+           </tr>
+         ))}
+       </tbody>
+      </table>
+
+      <button onClick={() => setShowAddForm(true)}>Add Species</button>
+      {showAddForm && (
+        <div>
+          <form>
+            <input value={newSpeciesName} onChange={(e) => setNewSpeciesName(e.target.value)} />
+            <input value={newSpeciesDescription} onChange={(e) => setNewSpeciesDescription(e.target.value)} />
+            <button onClick={addSpecies}>Add new species</button>
+            <button onClick={() =>setShowAddForm(false)}>Cancel</button>
+          </form>
+        </div>
+      )}
+    </div>
   )
 }
