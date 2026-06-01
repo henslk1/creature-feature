8409e365a0dd8a803fee767be55b46712246fbbe
Kristin <knhbjr@gmail.com>
2026-06-01 03:04:04 -0500

    Fetch species

--- frontend/src/pages/SpeciesProfilePage.tsx
+++ frontend/src/pages/SpeciesProfilePage.tsx
@@ -1,5 +1,23 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { API_URL } from "../config";
+import type { Species } from "../types";
+
 export function SpeciesProfilePage() {
-  return (
-    <div>Species</div>
-  )
+
+  const { speciesId } = useParams();
+
+  // GET
+  const [species, setSpecies] = useState<Species | null>(null);
+
+  // Landing page
+  useEffect(() => {
+    fetch(`${API_URL}/species/${speciesId}`)
+      .then(res => {
+        if(!res.ok) return;
+        return res.json();
+      })
+      .then(data => setSpecies(data))
+  }, []);
+
 }
