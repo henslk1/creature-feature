6d772e4f5cfd199b1df49c46ee068f1bdefb7c2e
Kristin <knhbjr@gmail.com>
2026-06-03 16:22:51 -0500

    const GENE_URL to shorten the fetch calls and reused code

--- frontend/src/hooks/useGeneSection.ts
+++ frontend/src/hooks/useGeneSection.ts
@@ -19,6 +19,7 @@ export function useGeneSection(
   const DEFAULT_ALLELE = { name: "", symbol: "", dominance: "", probability: "" };
   const DEFAULT_LOCUS = { name: "", alleles: [DEFAULT_ALLELE] };
   const DEFAULT_RULE = { minDominantAlleles: "", expression: ""};
+  const GENE_URL = `${API_URL}/species/${speciesId}/genes`;
   // Setters
   const [newGeneName, setNewGeneName] = useState("");
   const [newGeneCategory, setNewGeneCategory] = useState("");
@@ -49,7 +50,7 @@ export function useGeneSection(
 
   // POST
   function addGene() {
-    fetch(`${API_URL}/species/${speciesId}/genes`, {
+    fetch(`${GENE_URL}`, {
       method: "POST",
       headers: JSON_HEADERS,
       body: JSON.stringify({
@@ -82,7 +83,7 @@ export function useGeneSection(
 
   // PATCH
   function saveGene() {
-    fetch(`${API_URL}/species/${speciesId}/genes/${editingGene?.id}`, {
+    fetch(`${GENE_URL}/${editingGene?.id}`, {
       method: "PATCH",
       headers: JSON_HEADERS,
       body: JSON.stringify({
@@ -105,7 +106,7 @@ export function useGeneSection(
   function saveLocus() {
     const gene = genes.find(g => g.loci.some(l => l.id === editingLocus!.id))!;
     const updatedLoci = gene.loci.map(l => l.id === editingLocus!.id ? editingLocus! : l);
-    fetch(`${API_URL}/species/${speciesId}/genes/${editingLocus?.geneId}`, {
+    fetch(`${GENE_URL}/${editingLocus?.geneId}`, {
       method: "PATCH",
       headers: JSON_HEADERS,
       body: JSON.stringify({
@@ -126,7 +127,7 @@ export function useGeneSection(
   function saveRule() {
     const gene = genes.find(g => g.expressionRules.some(r => r.id === editingRule!.id))!;
     const updatedRule = gene.expressionRules.map(r => r.id === editingRule!.id ? editingRule! : r);
-    fetch(`${API_URL}/species/${speciesId}/genes/${editingRule?.geneId}`, {
+    fetch(`${GENE_URL}/${editingRule?.geneId}`, {
       method: "PATCH",
       headers: JSON_HEADERS,
       body: JSON.stringify({
@@ -146,7 +147,7 @@ export function useGeneSection(
 
   // DELETE
   function deleteGene(id: number) {
-    fetch(`${API_URL}/species/${speciesId}/genes/${id}`, {
+    fetch(`${GENE_URL}/${id}`, {
       method: "DELETE"
     })
     .then(res => {
@@ -157,7 +158,7 @@ export function useGeneSection(
 
   function deleteLocus(locusId: number, geneId: number) {
     const updatedLoci = genes.find(g => g.id === geneId)!.loci.filter(l => l.id !== locusId);
-    fetch(`${API_URL}/species/${speciesId}/genes/${geneId}`, {
+    fetch(`${GENE_URL}/${geneId}`, {
       method: "PATCH",
       headers: JSON_HEADERS,
       body: JSON.stringify({ loci: updatedLoci })
@@ -174,7 +175,7 @@ export function useGeneSection(
 
   function deleteRule(ruleId: number, geneId:number) {
     const updatedRule = genes.find(g => g.id === geneId)!.expressionRules.filter(r => r.id !== ruleId);
-    fetch(`${API_URL}/species/${speciesId}/genes/${geneId}`, {
+    fetch(`${GENE_URL}/${geneId}`, {
       method: "PATCH",
       headers: JSON_HEADERS,
       body: JSON.stringify({ expressionRules: updatedRule })
