dda7e9572b345c907063722cf84e706704c25f1e
Kristin <knhbjr@gmail.com>
2026-06-07 23:04:28 -0500

    Initial set up of breed profile

--- frontend/src/pages/BreedProfilePage.tsx
+++ frontend/src/pages/BreedProfilePage.tsx
@@ -1,5 +1,32 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { API_URL, JSON_HEADERS } from "../config";
+import type { Breed } from "../types";
+
 export function BreedProfilePage() {
+
+  const { breedId } = useParams();
+
+  const [breed, setBreed] = useState<Breed | null>(null);
+
+  // Landing page
+  useEffect(() => {
+    fetch(`${API_URL}/breeds/${breedId}`)
+      .then(res => {
+        if(!res.ok) return;
+        return res.json();
+      })
+      .then(data => setBreed(data))
+  }, []);
+
+  if(!breed) return <div>Loading...</div>
+
   return (
-    <div>Breed Profile</div>
+    <div>
+      
+      <h1>{breed.name}</h1>
+      <span>{breed.active ? "Active" : "Not Active"}</span>
+
+    </div>
   )
 }
